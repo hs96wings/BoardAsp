e4c2816b417d5ccfeb699d80ed676df6d069739e
hs96wings <hs96wings@gmail.com>
2025-08-25 12:14:08 +0900

    [Server]: CORS 추가

--- Board.Api/Program.cs
+++ Board.Api/Program.cs
@@ -1,11 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// --- CORS 서비스 추가 ---
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:7001") // Blazor 주소 수정 필요
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -19,6 +33,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// --- CORS 미들웨어 사용 ---
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
